d2bd6e2063cee3b9449cfb04e101988f76a42aaf
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-12-29 23:43:40 +0100

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, files) => {
+  for (const file of files) {
+    if (file.isDirectory()) {
+      continue;
+    }
+    
+    const extensionName = path.extname(file.name);
+    const fileName = path.basename(file.name, extensionName);
+    
+    try {
+      const stats = fs.statSync(`${file.path + '/' + file.name}`);
+      const size = stats.size/1000;
+      console.log(fileName + ' ' + '-' + ' ' + extensionName.slice(1) + ' ' + '-' + ' ' + size + 'kb');
+    } catch (err) {
+      console.error(err);
+    }
+    
+  }
+})
+
